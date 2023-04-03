111294acee3afff57c5dccfd4314a4683c339d46
ArchLinux <kovrevik1@gmail.com>
2023-04-03 10:32:29 -0400

    fixed the missing texture crash

--- level_loader.py
+++ level_loader.py
@@ -3,14 +3,13 @@ import definitions
 
 
 class level_loader:
-    def __init__ (self, surface: pygame.Surface, source_folder: str, definitions_pack: str = None):
+    def __init__ (self, surface: pygame.Surface, textures):
         '''
         Constructor for the level_loader class
         
         Args:
             surface: the surface which all assets will be drawn on
             source_folder: the base folder to search for the .lvl files
-            definitions_pack: pack that defines what images each key will load (Leave empty to use specified pack in each .lvl file)
             
         Returns:
             None
@@ -19,14 +18,9 @@ class level_loader:
             None
         '''
         self.surface = surface
-        self.source = source_folder
-        self.definitions_pack = definitions_pack
-        if (definitions_pack == None):
-            self.dynmaic_def_pack = False
-        else:
-            self.dynamic_def_pack = True 
+        self.textures = textures
 
-    def __load_def_pack(self):
+    def __load_textures(self):
         with open(self.definitions_pack, "r") as f:
             lines = f.readlines()
         
