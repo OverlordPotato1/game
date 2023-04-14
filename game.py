a0c9990aaec9b237b33a6028c5d5fe16bf5f16d0
ArchLinux <kovrevik1@gmail.com>
2023-04-14 09:53:27 -0400

    i dont know what im doing

--- game.py
+++ game.py
@@ -66,7 +66,7 @@ lvl_loader = level_loader(screen_view, "levels/", "textures.json", player_collid
 # Load a new level
 lvl_loader.new_lvl("test.lvl")
 
-
+frames = 0
 
 
 doTheThing = True
@@ -113,59 +113,65 @@ while doTheThing:
     # Update the scrolling position based on the key flags
     scroll_x, scroll_y = playerWalk(up, down, left, right, scroll_x, scroll_y)
 
-    for thing in player_collide_group:
-        thing_rect = thing[1]
+    if up or down or left or right:
+        for thing in player_collide_group:
+            thing_rect = thing[1]
 
-        # check for collision with player
-        player_rect = player.get_rect()
-        player_rect.center = (sw / 2, sh / 2)
-        if func.pixel_collision(player, player.get_rect(), thing[0], thing[1]):
+            # check for collision with player
+            player_rect = player.get_rect()
+            player_rect.center = (sw / 2, sh / 2)
+            if func.pixel_collision(player, player.get_rect(), thing[0], thing[1]):
 
-            # Calculate the overlap size for each direction
-            right_overlap = player.get_rect().right - thing_rect.left
-            left_overlap = thing_rect.right - player.get_rect().left
-            up_overlap = thing_rect.bottom - player.get_rect().top
-            down_overlap = player.get_rect().bottom - thing_rect.top
+                # Calculate the overlap size for each direction
+                right_overlap = player.get_rect().right - thing_rect.left
+                left_overlap = thing_rect.right - player.get_rect().left
+                up_overlap = thing_rect.bottom - player.get_rect().top
+                down_overlap = player.get_rect().bottom - thing_rect.top
 
-            # Find the smallest overlap
-            min_overlap = min(right_overlap, left_overlap, up_overlap, down_overlap)
+                # Find the smallest overlap
+                min_overlap = min(right_overlap, left_overlap, up_overlap, down_overlap)
 
-            # detect right side collision
-            if min_overlap == right_overlap:
-                print("hit right")
+                # detect right side collision
+                if min_overlap == right_overlap:
+                    print("hit right")
 
-            # detect left side collision
-            elif min_overlap == left_overlap:
-                print("hit left")
+                # detect left side collision
+                elif min_overlap == left_overlap:
+                    print("hit left")
 
-            # detect up collision
-            elif min_overlap == up_overlap:
-                print("hit up")
+                # detect up collision
+                elif min_overlap == up_overlap:
+                    print("hit up")
 
-            # detect down collision
-            elif min_overlap == down_overlap:
-                print("hit down")
+                # detect down collision
+                elif min_overlap == down_overlap:
+                    print("hit down")
+
+
+
+        # Draw red boxes around the objects that are collided with
+        for thing_rect in player_collide_group:
+            thing_screen_pos = ((thing[1].x + scroll_x), (thing[1].y + scroll_y))
+
+            if func.pixel_collision(player, player.get_rect(), thing[0], thing[1]):
+                red_box_surface = pygame.Surface((thing_rect.width, thing_rect.height), pygame.SRCALPHA)
+                red_box_surface.fill((0, 0, 0, 0))
+                pygame.draw.rect(red_box_surface, (255, 0, 0), red_box_surface.get_rect(), 1)
+                screen.blit(red_box_surface, thing_screen_pos)
+                player_hitbox = pygame.Surface((player_width, player_height), pygame.SRCALPHA)
+                player_hitbox.fill((0, 0, 0, 0))
+                pygame.draw.rect(player_hitbox, (170, 23, 255), player_hitbox.get_rect(), 2)
+                screen.blit(player_hitbox, ((sw/2) - (player_width/2), (sh/2)- (player_height/2)))
 
     # Draw the level surface
     screen.blit(lvl_loader.surface, (0, 0), (-scroll_x, -scroll_y, sw, sh))
 
-    # Draw red boxes around the objects that are collided with
-    for thing_rect in player_collide_group:
-        thing_screen_pos = ((thing[1].x + scroll_x), (thing[1].y + scroll_y))
-
-        if func.pixel_collision(player, player.get_rect(), thing[0], thing[1]):
-            red_box_surface = pygame.Surface((thing_rect.width, thing_rect.height), pygame.SRCALPHA)
-            red_box_surface.fill((0, 0, 0, 0))
-            pygame.draw.rect(red_box_surface, (255, 0, 0), red_box_surface.get_rect(), 1)
-            screen.blit(red_box_surface, thing_screen_pos)
-            player_hitbox = pygame.Surface((player_width, player_height), pygame.SRCALPHA)
-            player_hitbox.fill((0, 0, 0, 0))
-            pygame.draw.rect(player_hitbox, (170, 23, 255), player_hitbox.get_rect(), 2)
-            screen.blit(player_hitbox, ((sw/2) - (player_width/2), (sh/2)- (player_height/2)))
-
     screen.blit(player, ((sw/2)-(player_width/2), (sh/2)-(player_height/2)), (0, 0, sw, sh))
-
-    current_fps = round(clock.get_fps(), 10)
+    try:
+        if frames % (int(clock.get_fps()/2)) == 0:
+            current_fps = int(clock.get_fps())
+    except:
+        current_fps = 2147483647
 
     fps_text = font.render("FPS: {}".format(current_fps), True, pygame.Color('white'))
 
@@ -175,5 +181,7 @@ while doTheThing:
     pygame.display.flip()
 
     clock.tick(fps)
+
+    frames += 1
 
 pygame.quit()
